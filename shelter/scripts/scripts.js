b927b7a8f07860eb625e418a24f075cf6978e0a4
GreyDraco <ancientgreydragon@gmail.com>
2024-09-06 06:42:05 +0300

    feat: add burger menu content

--- shelter/scripts/scripts.js
+++ shelter/scripts/scripts.js
@@ -1,8 +1,27 @@
 console.log("Script is connected!");
 
 const overlay = document.querySelector(".overlay");
-const burger_menu = document.querySelector(".burger-menu");
-burger_menu.addEventListener("click", () => {
-  burger_menu.classList.toggle("burger-menu_active");
+const burgerMenuBtn = document.querySelector(".burger-menu-btn");
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenuBtn.addEventListener("click", () => {
+  burgerMenuBtn.classList.toggle("burger-menu-btn_active");
   overlay.classList.toggle("overlay_active");
+  burgerMenu.classList.toggle("burger-menu_active");
 });
+
+overlay.addEventListener("click", () => {
+  burgerMenuBtn.classList.remove("burger-menu-btn_active");
+  overlay.classList.remove("overlay_active");
+  burgerMenu.classList.remove("burger-menu_active");
+});
+
+function handleResize() {
+  if (window.innerWidth > 767) {
+    burgerMenuBtn.classList.remove("burger-menu-btn_active");
+    overlay.classList.remove("overlay_active");
+    burgerMenu.classList.remove("burger-menu_active");
+  }
+}
+
+window.addEventListener("resize", handleResize);
