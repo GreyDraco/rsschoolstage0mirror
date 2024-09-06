311198f0cc7bcff27896d6fe2ab48db10a59df57
GreyDraco <ancientgreydragon@gmail.com>
2024-09-06 07:57:55 +0300

    feat: add scroll lock when burger menu open

--- shelter/scripts/scripts.js
+++ shelter/scripts/scripts.js
@@ -3,17 +3,29 @@ console.log("Script is connected!");
 const overlay = document.querySelector(".overlay");
 const burgerMenuBtn = document.querySelector(".burger-menu-btn");
 const burgerMenu = document.querySelector(".burger-menu");
+const burgerMenuList = document.querySelector(".burger-menu-list");
+const body = document.body;
 
 burgerMenuBtn.addEventListener("click", () => {
   burgerMenuBtn.classList.toggle("burger-menu-btn_active");
   overlay.classList.toggle("overlay_active");
   burgerMenu.classList.toggle("burger-menu_active");
+  body.style.overflow = "hidden";
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
 overlay.addEventListener("click", () => {
   burgerMenuBtn.classList.remove("burger-menu-btn_active");
   overlay.classList.remove("overlay_active");
   burgerMenu.classList.remove("burger-menu_active");
+  body.style.overflow = "auto";
+});
+
+burgerMenuList.addEventListener("click", () => {
+  burgerMenuBtn.classList.remove("burger-menu-btn_active");
+  overlay.classList.remove("overlay_active");
+  burgerMenu.classList.remove("burger-menu_active");
+  body.style.overflow = "auto";
 });
 
 function handleResize() {
@@ -21,6 +33,7 @@ function handleResize() {
     burgerMenuBtn.classList.remove("burger-menu-btn_active");
     overlay.classList.remove("overlay_active");
     burgerMenu.classList.remove("burger-menu_active");
+    body.style.overflow = "auto";
   }
 }
 
