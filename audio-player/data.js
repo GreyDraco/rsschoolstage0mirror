f54e3ecab6e64132bd8951021fcb3d5580a9ae72
GreyDraco <ancientgreydragon@gmail.com>
2024-09-16 11:43:09 +0300

    feat: add footer

---
+++ audio-player/data.js
@@ -0,0 +1,14 @@
+export default data = [
+  {
+    src: "./assets/audio/beyonce.mp3",
+    img: "./assets/img/lemonade.png",
+    singer: "Beyonce",
+    title: "Don't Hurt Yourself",
+  },
+  {
+    src: "./assets/audio/dontstartnow.mp3",
+    img: "./assets/img/dontstartnow.png",
+    singer: "Dua Lipa",
+    title: "Don't Start Now",
+  },
+];
