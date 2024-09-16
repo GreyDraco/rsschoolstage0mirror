56aeb512a68cc8feadac97fbbc4c155d190afd6d
GreyDraco <ancientgreydragon@gmail.com>
2024-09-16 22:19:16 +0300

    feat: add styles and image change

--- audio-player/script.js
+++ audio-player/script.js
@@ -12,9 +12,16 @@ const progressBar = document.querySelector(".progress-bar");
 const singerName = document.querySelector(".singer");
 const trackName = document.querySelector(".track");
 const labelName = document.querySelector(".label");
+const runningInfo = document.querySelector(".track-info");
+const trackInfoContainer = document.querySelector(".running-info");
+const miniImg = document.querySelector(".mini-img");
+const bigImg = document.querySelector(".back-img");
+
+const maxTrackInfoWidth = trackInfoContainer.offsetWidth + 280;
 
 let currentTrack = 0;
 const audio = new Audio(data[currentTrack].src);
+updateTrack();
 
 playBtn.addEventListener("click", () => {
   if (!playBtn.classList.contains("pause")) {
@@ -23,6 +30,7 @@ playBtn.addEventListener("click", () => {
     audio.pause();
   }
   playBtn.classList.toggle("pause");
+  miniImg.classList.toggle("magnified");
 });
 
 audio.addEventListener("loadeddata", () => {
@@ -50,8 +58,13 @@ progressBar.addEventListener("change", () => {
 });
 
 volumeBar.addEventListener("change", () => {
-  if (volumeBtn.classList.contains("mute")) {
+  if (volumeBar.value < 1) {
+    volumeBtn.classList.add("mute");
+    volumeControl.classList.add("mute");
+    audio.volume = 0;
+  } else if (volumeBtn.classList.contains("mute") && volumeBar.value > 0) {
     volumeBtn.classList.remove("mute");
+    volumeControl.classList.remove("mute");
     audio.volume = volumeBar.value / 100;
   } else {
     audio.volume = volumeBar.value / 100;
@@ -59,6 +72,9 @@ volumeBar.addEventListener("change", () => {
 });
 
 volumeBtn.addEventListener("click", () => {
+  if (volumeBar.value < 1 && volumeBtn.classList.contains("mute")) {
+    volumeBar.value = 15;
+  }
   volumeBtn.classList.toggle("mute");
   volumeControl.classList.toggle("mute");
   audio.volume = volumeBtn.classList.contains("mute") ? 0 : volumeBar.value / 100;
@@ -69,8 +85,14 @@ function updateTrack(id = currentTrack) {
   singerName.textContent = data[currentTrack].singer;
   trackName.textContent = data[currentTrack].title;
   labelName.textContent = data[currentTrack].label;
+  miniImg.src = data[currentTrack].img;
+  bigImg.src = data[currentTrack].img;
+
   if (playBtn.classList.contains("pause")) audio.play();
+
   audio.volume = volumeBtn.classList.contains("mute") ? 0 : volumeBar.value / 100;
+
+  checkRunningAnim();
 }
 
 function formatTime(time) {
@@ -83,9 +105,24 @@ function formatTimeDecimals(time) {
   return time >= 10 ? `${time}` : `0${time}`;
 }
 
+function checkRunningAnim() {
+  if (runningInfo.offsetWidth >= maxTrackInfoWidth) {
+    runningInfo.classList.add("animated");
+  } else {
+    runningInfo.classList.remove("animated");
+  }
+}
+
 function updateCurrentState() {
   currentTime.textContent = formatTime(audio.currentTime);
   progressBar.value = Math.floor((audio.currentTime / audio.duration) * 100);
+  if (audio.currentTime === audio.duration) {
+    currentTrack++;
+    if (currentTrack >= data.length) {
+      currentTrack = 0;
+    }
+    updateTrack(currentTrack);
+  }
 }
 
 setInterval(updateCurrentState, 500);
