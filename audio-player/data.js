2e5277c5fd685a3cbf8a830a981dda9d14d2c6ca
GreyDraco <ancientgreydragon@gmail.com>
2024-09-16 14:28:23 +0300

    feat: add basic layout

--- audio-player/data.js
+++ audio-player/data.js
@@ -4,11 +4,13 @@ export default data = [
     img: "./assets/img/lemonade.png",
     singer: "Beyonce",
     title: "Don't Hurt Yourself",
+    label: "Lemonade",
   },
   {
     src: "./assets/audio/dontstartnow.mp3",
     img: "./assets/img/dontstartnow.png",
     singer: "Dua Lipa",
     title: "Don't Start Now",
+    label: "Future Nostalgia",
   },
 ];
