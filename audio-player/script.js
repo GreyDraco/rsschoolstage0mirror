5241d8a84948a296ef834bd07745ba26dd0e41dd
GreyDraco <ancientgreydragon@gmail.com>
2024-09-16 18:23:46 +0300

    feat: add progress bar functionality

--- audio-player/script.js
+++ audio-player/script.js
@@ -3,8 +3,12 @@ const pauseBtn = document.querySelector(".pause");
 const prevBtn = document.querySelector(".prev");
 const nextBtn = document.querySelector(".next");
 const volumeBtn = document.querySelectorAll(".volume-btn");
+const duration = document.querySelector(".duration");
+const currentTime = document.querySelector(".current-time");
+const progressBar = document.querySelector(".progress-bar");
 
-var audio = new Audio("./assets/audio/beyonce.mp3");
+const audio = new Audio("./assets/audio/beyonce.mp3");
+let audioDuration = 0;
 
 playBtn.addEventListener("click", () => {
   if (!playBtn.classList.contains("pause")) {
@@ -14,3 +18,33 @@ playBtn.addEventListener("click", () => {
   }
   playBtn.classList.toggle("pause");
 });
+
+audio.addEventListener("loadeddata", () => {
+  console.log(formatTime(audio.duration));
+  duration.textContent = formatTime(audio.duration);
+});
+
+nextBtn.addEventListener("click", () => {
+  console.log(audio.currentTime);
+});
+
+progressBar.addEventListener("change", () => {
+  audio.currentTime = Math.floor((progressBar.value * audio.duration) / 100);
+});
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${formatTimeDecimals(minutes)}:${formatTimeDecimals(seconds)}`;
+}
+
+function formatTimeDecimals(time) {
+  return time >= 10 ? `${time}` : `0${time}`;
+}
+
+function updateCurrentState() {
+  currentTime.textContent = formatTime(audio.currentTime);
+  progressBar.value = Math.floor((audio.currentTime / audio.duration) * 100);
+}
+
+setInterval(updateCurrentState, 500);
