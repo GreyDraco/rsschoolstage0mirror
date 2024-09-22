0ca7f2c0dca72fd4fcc3ff413c51cd8ed520ac84
GreyDraco <ancientgreydragon@gmail.com>
2024-09-22 15:23:26 +0300

    feat: add search bar logic

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -1,6 +1,11 @@
 import { fakeData, fakeDataLandscape, fakeDataLandscape2, fakeDataPort, fakeDataRandom, fakeDataSquar } from "./fake-data.js";
 
 const main = document.querySelector(".main");
+const searchBtn = document.querySelector(".search-btn");
+const clearBtn = document.querySelector(".clear-btn");
+const searchBar = document.querySelector(".search-bar");
+
+let lastRequest = "";
 
 fakeData.forEach((res, index) => {
   const url = res.urls.small;
@@ -19,4 +24,31 @@ fakeData.forEach((res, index) => {
   main.append(imgContainer);
 
   imgContainer.style.gridRowEnd = `span ${Math.ceil(newH / 10)} `;
+});
+
+searchBar.addEventListener("input", () => {
+  if (searchBar.value) {
+    clearBtn.classList.remove("hidden");
+  } else {
+    clearBtn.classList.add("hidden");
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  searchBar.value = "";
+  clearBtn.classList.add("hidden");
+});
+
+searchBtn.addEventListener("click", () => {
+  if (searchBar.value) {
+    console.log("searching....");
+  }
+});
+
+searchBar.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    if (searchBar.value) {
+      console.log("searching....");
+    }
+  }
 });
