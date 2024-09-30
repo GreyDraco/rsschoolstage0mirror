6b91f8f2c94d3dfbed022acb2af2a5fc0399b549
GreyDraco <ancientgreydragon@gmail.com>
2024-09-30 23:38:24 +0300

    feat: add gold

--- random-game/script.js
+++ random-game/script.js
@@ -1,12 +1,33 @@
 import { ctx } from "./scripts/canvasSetup.js";
 import { MovingSprite, Sprite } from "./scripts/classes/Sprites.js";
-import { CANVAS_HEIGHT, CANVAS_WIDTH, CASTLE_PROPS, HP_CASTLE_POS, HP_ENEMIES_POS, HP_HEIGHT, HP_MAX_WIDTH, HP_Y_POS, MAX_CASTLE_HP, MAX_ENEMY_HP, START_ENEMIES_PROPS } from "./scripts/consts.js";
+import { CANVAS_HEIGHT, CANVAS_WIDTH, CASTLE_PROPS, HP_CASTLE_POS, HP_ENEMIES_POS, HP_HEIGHT, HP_MAX_WIDTH, HP_Y_POS, MAX_CASTLE_HP, MAX_ENEMY_HP, START_ENEMIES_PROPS, STOP_ENEMY_POS } from "./scripts/consts.js";
 
 const castle = new Sprite(CASTLE_PROPS);
 const enemyCount = 20;
+const castleHitCount = 4;
+const castleRange = 300;
 let damageRecived = 0;
+let gold = 100;
 
 let enemies = [];
+
+function displayGold() {
+  ctx.beginPath();
+  ctx.arc(CANVAS_WIDTH / 2 - 100, HP_Y_POS - 15, 20, 0, 2 * Math.PI);
+  ctx.fillStyle = "yellow";
+  ctx.fill();
+  ctx.lineWidth = 4;
+  ctx.strokeStyle = "#ecec73";
+  ctx.stroke();
+
+  ctx.font = "30px Arial";
+  ctx.fillStyle = "purple";
+  ctx.fillText("$", CANVAS_WIDTH / 2 - 107, HP_Y_POS - 3);
+
+  ctx.font = "40px Arial";
+  ctx.fillStyle = "purple";
+  ctx.fillText(gold, CANVAS_WIDTH / 2 - 70, HP_Y_POS);
+}
 
 function spawnEnemy(count = enemyCount) {
   let counter = 0;
@@ -44,17 +65,23 @@ function displayHP() {
 }
 
 function hitClosestEnemies(damage) {
+  let damageRecived1 = 0;
   enemies.sort((a, b) => a.xPos - b.xPos);
-  const closestEnemies = enemies.slice(0, Math.min(3, enemies.length));
+  const closestEnemies = enemies.slice(0, Math.min(castleHitCount, enemies.length));
   closestEnemies.forEach((enemy) => {
-    enemy.hp = Math.max(0, enemy.hp - damage / Math.min(3, enemies.length));
+    if (enemy.xPos < STOP_ENEMY_POS + castleRange) {
+      enemy.hp = Math.max(0, enemy.hp - damage);
+      damageRecived1 += damage;
+    }
   });
   enemies = enemies.filter((enemy) => enemy.hp > 0);
+  return damageRecived1;
 }
 
 function updateEnemyHp(damage) {
-  damageRecived += damage;
-  hitClosestEnemies(damage);
+  // damageRecived += damage;
+  // hitClosestEnemies(damage);
+  damageRecived += hitClosestEnemies(damage);
   const currentEnemHPWidth = (1 - (getEnemyMaxHp() - damageRecived) / getEnemyMaxHp()) * HP_MAX_WIDTH;
   enemiesHP.width = Math.min(currentEnemHPWidth, HP_MAX_WIDTH);
 }
@@ -78,16 +105,22 @@ function animate() {
       enemy.move();
     } else {
       updateCastleHp(enemy.power);
-      updateEnemyHp(castle.power);
       enemy.display();
     }
   });
+
+  if (enemies.length) {
+    updateEnemyHp(castle.power);
+  }
+
+  displayGold();
 
   ctx.beginPath();
   ctx.moveTo(CASTLE_PROPS.width + 50, CANVAS_HEIGHT);
   ctx.lineTo(CASTLE_PROPS.width + 50, 0);
   ctx.stroke();
 }
+
 animate();
 
 function getRandomColorWithOpacity() {
